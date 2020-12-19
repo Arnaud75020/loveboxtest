c935c6f31efba899a27d49c287f723c5052f9bf1
Arnaud Duhé <anro010@hotmail.com>
2020-12-19 21:50:21 +0000

    style

--- src/client/Home.jsx
+++ src/client/Home.jsx
@@ -107,7 +107,7 @@ export default () => {
                         });
                       }}
                     >
-                      {isRed ? '' : 'x'}
+                      {isRed ? '' : 'X'}
                     </button>
                   </li>
                 </div>
